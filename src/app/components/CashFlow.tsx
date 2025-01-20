3dbe39856098647cd85f4bc5529be81247efa2f0
rafael-rod <133426209+rafael-rod@users.noreply.github.com>
2025-01-19 23:03:25 -0500

    Update CashFlow.tsx

--- src/app/components/CashFlow.tsx
+++ src/app/components/CashFlow.tsx
@@ -251,7 +251,7 @@ export default function CashFlow({ ticker }: CashFlowProps) {
       setLoading(true);
       setError(null);
       try {
-        const response = await fetch(`http://localhost:5000/api/financials/${ticker}`);
+        const response = await fetch(`https://stock-market-financial-statements-gareqa73p.vercel.app/${ticker}`);
         if (!response.ok) {
           throw new Error('Failed to fetch data');
         }
