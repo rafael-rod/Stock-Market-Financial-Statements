e72a6a7c56c052e26903380af5e9f0781a2d7ffe
rafael-rod <133426209+rafael-rod@users.noreply.github.com>
2025-01-20 01:25:53 -0500

    Update IncomeStatement.tsx

--- src/app/components/IncomeStatement.tsx
+++ src/app/components/IncomeStatement.tsx
@@ -288,7 +288,7 @@ export default function IncomeStatement({ ticker }: IncomeStatementProps) {
       setLoading(true);
       setError(null);
       try {
-        const response = await fetch(`https://stock-market-financial-statements-api.vercel.app///${ticker}`);
+        const response = await fetch(`https://stock-market-financial-statements-api.vercel.app/${ticker}`);
         if (!response.ok) {
           throw new Error('Failed to fetch data');
         }
